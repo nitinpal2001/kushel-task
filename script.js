35668259abe3244472671cad56add9a6ad76984c
Nitin Pal <nitinpalrav@gmail.com>
2023-04-06 21:14:53 +0530

    faq and contactus changes

--- script.js
+++ script.js
@@ -2,7 +2,35 @@ const mobileNavBtn = document.querySelector('.mobile-navbar-btn');
 const container = document.querySelector('.container');
 
 mobileNavBtn.addEventListener('click', () => {
-    container.classList.toggle('active');
+    container.classList.toggle('activeNav');
 }
 );
+
+// For Accordian FAQ section
+
+const accordian = document.querySelectorAll('.faq-container__box');
+const faqButton = document.querySelectorAll('.faq-container__box__heading__button');
+
+for (let i = 0; i < accordian.length; i++) {
+    faqButton[i].addEventListener('click', function () {
+        accordian[i].classList.toggle('activeFaq');
+    });
+}
+
+// For Toggling between Address and Map
+
+const addressButton = document.querySelector('.address-btn');
+const mapButton = document.querySelectorAll('.map-btn');
+const addressContainer = document.querySelector('.contactus-container__right__details');
+const mapContainer = document.querySelector('.contactus-container__right__map');
+
+addressButton.addEventListener('click', () => {
+    addressContainer.classList.add('active');
+    mapContainer.classList.remove('active');
+});
+
+
+
+
+
 
